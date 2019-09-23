f5923b3a9d78cc8e3ad125e2695638dd274cb653
Свирепа Денис Николаевич <denisns@ipps.by>
2019-09-23 10:54:34 +0300

    end of sect 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -95,10 +97,11 @@ const ActivityForm: React.FC<IProps> = ({
           value={activity.venue}
         />
         <Button
+          loading={submitting}
           floated="right"
           positive
           type="submit"
-          content="Sybmit"
+          content="Submit"
         ></Button>
         <Button
           onClick={() => setEditMode(false)}
