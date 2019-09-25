189958f79d99939c9f7b26cd67ca37d0de5eb338
Свирепа Денис Николаевич <denisns@ipps.by>
2019-09-25 09:45:30 +0300

    End of sect 9

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Segment, Header, Button, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <Container style={{ marginTop: "7em" }}>
-      <h1>Home page</h1>
-      <h3>
-        Go to <Link to="/activities">Activities</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Единый Реестр Лицензий
+        </Header>
+        <Header as="h2" inverted content="Добро пожаловать в ЕРЛ" />
+        <Button as={Link} to="/activities" size="huge" inverted>
+          Перейти к реестру лицензий!
+        </Button>
+      </Container>
+    </Segment>
   );
 };
 
