487dee212bbbe491ea7ca429ff0d68b3ff4e649d
Свирепа Денис Николаевич <denisns@ipps.by>
2019-09-25 15:30:46 +0300

    enf of sect 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,13 +21,13 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
-            return await _mediator.Send(new List.Query());   
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async  Task<ActionResult<Activity>> Details( Guid id)
+        public async Task<ActionResult<Activity>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query{Id = id});
+            return await _mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
@@ -46,7 +46,7 @@ namespace API.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command{Id= id});
+            return await _mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using MediatR;
 using Persistence;
 
@@ -9,34 +11,34 @@ namespace Application.Activities
     public class Delete
     {
         public class Command : IRequest
-                {
-                   public Guid Id { get; set; }
-                }
-        
-                public class Handler : IRequestHandler<Command>
-                {
-                    private readonly DataContext _context;
-                    public Handler(DataContext context)
-                    {
-                        this._context = context;
-        
-                    }
-        
-                    public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-                    {
-                       var activity  = await _context.Activities.FindAsync(request.Id);
+        {
+            public Guid Id { get; set; }
+        }
 
-                       if(activity == null)
-                            throw new Exception("Could not find Activity");
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                this._context = context;
 
-                        _context.Remove(activity);
+            }
 
-                        var success = await _context.SaveChangesAsync() > 0;
-        
-                        if (success) return Unit.Value;
-        
-                        throw new Exception("Problem saving changes");
-                    }
-                }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
+
+                _context.Remove(activity);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
     }
 }
